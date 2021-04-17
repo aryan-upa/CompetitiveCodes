e9fab555737161cbcb08064184f0c90bdd9ae783
Aryan Upadhyay <73682643+aryan-upa@users.noreply.github.com>
2021-04-17 15:55:06 +0530

    Anton and Letters

---
+++ AntonandLetters.py
@@ -0,0 +1,26 @@
+# Round 253, Div 2, Problem A
+# https://codeforces.com/contest/443/problem/A
+
+a = list(input())
+b = []
+for i in range(0, len(a)):
+    if a[i] == '{' or a[i] == ',' or a[i] == '}' or a[i] == ' ':
+        pass
+    else:
+        b.append(a[i])
+c = []
+for i in range(0, len(b)):
+    q = b[i]
+    flag = 1
+    for k in range(0, len(c)):
+        if q == c[k]:
+            flag = 0
+            break
+        else:
+            pass
+    if flag == 1:
+        c.append(b[i])
+    else:
+        pass
+
+print(len(c))
