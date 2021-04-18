b3d6d216a00e9bf564b4f01ec92478e8bc2cf6ff
Aryan Upadhyay <73682643+aryan-upa@users.noreply.github.com>
2021-04-18 23:07:53 +0530

    Product

---
+++ Product(UVA).py
@@ -0,0 +1,10 @@
+# UVA 10106 Product
+# https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=1047
+
+while True:
+    try:
+        x = int(input())
+        y = int(input())
+        print(x*y)
+    except EOFError:
+        break
