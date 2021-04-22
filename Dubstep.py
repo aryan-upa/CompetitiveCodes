31cb5a652ce35e59572f47809f5b2f98971a698b
Aryan Upadhyay <73682643+aryan-upa@users.noreply.github.com>
2021-04-22 10:51:42 +0530

    Dubstep

---
+++ Dubstep.py
@@ -0,0 +1,9 @@
+# Round 130, Div 2, Problem A
+# https://codeforces.com/contest/208/problem/A
+
+inp = input().split('WUB')
+for i in range(len(inp)):
+    if inp[i] == '':
+        pass
+    else:
+        print(inp[i], end=' ')
