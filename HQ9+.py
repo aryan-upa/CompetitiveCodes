85e6b8082a141580f9a370aee5230b3013caa39e
Aryan Upadhyay <73682643+aryan-upa@users.noreply.github.com>
2021-05-18 23:27:26 +0530

    HQ9+

---
+++ HQ9+.py
@@ -0,0 +1,8 @@
+# Beta Round 96, Div 2, Problem A
+# https://codeforces.com/contest/133/problem/A
+
+inp = input()
+if 'H' in inp or 'Q' in inp or '9' in inp:
+    print('YES')
+else:
+    print('NO')
