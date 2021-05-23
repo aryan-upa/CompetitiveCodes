fcb84ec12d572dcf9f6cdb814675d48a48254813
Aryan Upadhyay <73682643+aryan-upa@users.noreply.github.com>
2021-05-23 21:44:20 +0530

    Little Elephant and Function

---
+++ ElephantandFunction.py
@@ -0,0 +1,10 @@
+# Round 136, Div 2, Problem A
+# http://codeforces.com/contest/221/problem/A
+
+inp = int(input())
+if inp!=1:
+    print(inp,end=' ')
+    for q in range(1,inp):
+        print(q,end=' ')
+else:
+    print(1)
